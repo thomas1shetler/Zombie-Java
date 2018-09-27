bbbc68833a6b95d7c9865d6e10129541c73c8f7a
Student <19thomas.sh@student.sfsd.us>
2018-09-27 07:46:48 -0700

    Zombie 0.0.1

--- Zombie Game/src/Game.java
+++ Zombie Game/src/Game.java
@@ -7,11 +7,23 @@ public class Game extends Canvas implements Runnable {
 	public static int WIDTH = 800, HEIGHT = 608;
 	public String title = "Zombie Game";
 	
+	private Thread thread;
+	private boolean isRunning = false;	 
+	//is game running or not?
+	
 	public Game() {
 		//Constructor
 		new Window(WIDTH, HEIGHT, title, this);
 	}
 	
+	private synchronized void start (){
+		if(isRunning) return;
+		
+	}
+	
+	private synchronized void stop() {
+		if(!isRunning) return; 
+	}
 	//Game Loop 
 	public void run() {
 		
