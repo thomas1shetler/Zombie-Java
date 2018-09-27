846e2cc3712596857fb5374ab8baba5dff13cbf2
Student <19thomas.sh@student.sfsd.us>
2018-09-27 08:21:57 -0700

    Zombie 0.0.1

--- Zombie Game/src/Game.java
+++ Zombie Game/src/Game.java
@@ -1,6 +1,11 @@
 import java.awt.*;
 import java.awt.Canvas;
 
+//2D Zombie Survival
+//coded by Thomas Shetler
+//intial Dev version 0.0.1
+
+
 
 public class Game extends Canvas implements Runnable {
 
@@ -17,19 +22,67 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	private synchronized void start (){
+		//starting game
 		if(isRunning) return;
 		
+		thread = new Thread(this);
+		thread.start();
+		isRunning = true;
+				
 	}
 	
 	private synchronized void stop() {
+		//stopping game
 		if(!isRunning) return; 
+		
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		isRunning = false;
+		
 	}
 	//Game Loop 
 	public void run() {
-		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(isRunning){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+					
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+				//pre-made start up loop made by 
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
 		
 	}
-
+	
+	private void render() {
+		
+	}
+	
 	public static void main(String args[]) {
 		new Game();
 	}
